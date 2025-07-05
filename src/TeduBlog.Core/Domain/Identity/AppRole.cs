1b425583a0bb8609733869501fb832f9296c8f3a
Tuan Tran <tranquoctuanitsp@gmail.com>
2025-07-05 14:58:28 +0700

    Add entity framwork ,migration and seed data

---
+++ src/TeduBlog.Core/Domain/Identity/AppRole.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduBlog.Core.Domain.Identity
+{
+    public class AppRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(200)]
+        public required string DisplayName { get; set; }
+    }
+}
